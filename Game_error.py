1a22e891b5dbe499a300579490ed5ec27bf73fd7
Blood Marillion <bmarilli@er-c3.kzn.21-school.ru>
2021-07-24 23:42:52 +0300

    adds

--- Game_error.py
+++
@@ -1,8 +0,0 @@
-from Color import *
-
-class Game_Error(Exception):
-    def __init__(self, text):
-        tmp = RED + text + FONE
-        self.txt = tmp
-    def __str__(self):
-        return self.txt
